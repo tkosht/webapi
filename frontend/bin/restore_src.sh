cfa1b52a1bfe974a3d6630107087145ac8a9ccc1
tkosht <takehito.oshita.business@gmail.com>
2021-03-27 16:34:03 +0900

    mod: to refactor the `bin` directory

--- frontend/bin/restore_src.sh
+++ frontend/bin/restore_src.sh
@@ -3,5 +3,5 @@
 d=$(cd $(dirname $0) && pwd)
 cd $d/../
 
-git checkout frontend/src frontend/.eslintrc.js
+git checkout src .eslintrc.js
 git status | egrep deleted | awk '{print $NF}' | xargs git checkout
